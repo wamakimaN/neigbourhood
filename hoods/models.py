c6a7ede5d2b99d689f7ac796b6be9770d56fa851
wamakimaN <wamamoses@gmail.com>
2020-02-26 09:48:56 +0300

    add profile model

--- hoods/models.py
+++ hoods/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+  user = models.OneToOneField(User, on_delete = models.CASCADE)
+  bio = models.TextField(max_length = 100, blank = True)
+  profile_pic = models.ImageField(default= 'default.jpg', upload_to='profile_pics')
+  
+  def __str__(self):
+    return f'{self.user.username} Profile'
