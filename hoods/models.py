4baa974505022b59169eb06408eb38adce32e436
wamakimaN <wamamoses@gmail.com>
2020-02-26 10:11:54 +0300

    add neigborhood model

--- hoods/models.py
+++ hoods/models.py
@@ -5,6 +5,15 @@ class Profile(models.Model):
   user = models.OneToOneField(User, on_delete = models.CASCADE)
   bio = models.TextField(max_length = 100, blank = True)
   profile_pic = models.ImageField(default= 'default.jpg', upload_to='profile_pics')
+  business = models.ForeignKey(Business, null = True, blank = True)
   
   def __str__(self):
     return f'{self.user.username} Profile'
+
+class Neighborhood(models.Model):
+  name = models.TextField(max_length = 100, blank = True)
+  health = models.TextField(max_length = 200)
+  police = models.TextField(max_length = 200)
+
+
+
--- neigbour/settings.py
+++ neigbour/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-  'hoods.apps.HoodsConfig',
+    'hoods.apps.HoodsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
